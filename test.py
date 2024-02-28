7c50e3fedb932bb4f3ae5628fbb1908fc2854bb0
Teng Hung Yi <109363799+Wilson-Teng1029@users.noreply.github.com>
2024-02-28 19:43:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
-from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
- 
+
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
